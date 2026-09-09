a2f99f370e63ba1f23645e82068f87620289bb0e
shivanshferro <shivanshferro@gmail.com>
2026-09-09 22:12:20 +0530

    Sixth commit of C

--- demo.c
+++ demo.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-int main(){
-    int x;
-    x = 5;
-    printf("%d",   x);
-    x = 7;
-    printf("%d", x);
-    x = x + 6;
-    printf("%d", x);
-    x = x -20;
-    printf("%d",x);
+int main()
+{
+    int x = 5;
+    int y = 4;
+    printf("%d",x+y);
+    printf("%d",x-y);
+    printf("%d",x*y);
+    printf("%d",x/y);
     return 0;
+
 }
