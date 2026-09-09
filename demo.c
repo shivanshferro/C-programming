79515f52c4cbb312266567c4aaecc49d129eb19e
shivanshferro <shivanshferro@gmail.com>
2026-09-09 22:16:19 +0530

    seventh commit of c

--- demo.c
+++ demo.c
@@ -3,10 +3,11 @@ int main()
 {
     int x = 5;
     int y = 4;
-    printf("%d",x+y);
-    printf("%d",x-y);
-    printf("%d",x*y);
-    printf("%d",x/y);
+    printf("%d\n",x+y);
+    printf("%d\n",x-y);
+    printf("%d\n",x*y);
+    printf("%d\n",x/y);
+    printf("%d\n",x);
     return 0;
 
 }
