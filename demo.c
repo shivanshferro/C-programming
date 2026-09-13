9ed102d90e16c3fac8f2d9972f36b59dba49edc8
shivanshferro <shivanshferro@gmail.com>
2026-09-13 22:25:39 +0530

    13 commit of C

--- demo.c
+++ demo.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-int main()
-{
-    int x = 5;
-    int y = 4;
-    printf("%d\n",x+y);
-    printf("%d\n",x-y);
-    printf("%d\n",x*y);
-    printf("%d\n",x/y);
-    printf("%d\n",x);
+int main (){
+    float maths = 20;
+    float evs = 40;
+    float science = 30;
+    float hindi = 30;
+    float total = maths + evs + science + hindi;
+    float percentage = (total/160)*100;
+    printf(" %f",percentage);
     return 0;
-
 }
--- precentage.c
+++ precentage.c
@@ -7,7 +7,7 @@ int main (){
     float marks5 = 60;  //physical education
     float totalmarks = marks1 + marks2 + marks3 + marks4 +marks5;
     float percentage = (totalmarks/500)*100;
-    printf("%f",percentage);
+    printf(" %f",percentage);
     return 0;
 
 }
