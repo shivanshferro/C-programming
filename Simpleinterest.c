1f6a24350ada4fe0610d7dee99987c1fc4c3d7b2
shivanshferro <shivanshferro@gmail.com>
2026-09-14 20:34:33 +0530

    14 commit of C

---
+++ Simpleinterest.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    float  p, r, t;
+    p = 1000;
+    r = 5;
+    t = 2;
+    float simple_interest = (p * r *t)/100;
+    printf("%f",simple_interest);
+    return 0;
+}
