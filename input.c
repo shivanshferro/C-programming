4ac3d7d94bb71eefb30ddceae36bc63b22d6b97c
shivanshferro <shivanshferro@gmail.com>
2026-09-16 23:56:15 +0530

    17 commit of C

---
+++ input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("enter the number : ");
+    scanf("%d",&x);
+    int y;
+    printf("enter the number : ");
+    scanf("%d",&y);
+    printf("%d",x+y);
+    return 0;
+}
