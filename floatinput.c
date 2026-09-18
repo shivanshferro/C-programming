c3a96084978b00a222e36726346fbc1604770b54
shivanshferro <shivanshferro@gmail.com>
2026-09-18 21:34:31 +0530

    22 commit of C

---
+++ floatinput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    float a;
+    printf("enter the number : ");
+    scanf("%f",&a);
+    int b;
+    b = a;
+    float z;
+    z = a - b;
+    printf("%f",z);
+    return 0;
+}
