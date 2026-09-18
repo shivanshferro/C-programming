a3026dc02f04c5fab6dc08c3d7908ed508c20963
shivanshferro <shivanshferro@gmail.com>
2026-09-18 20:58:51 +0530

    21 commit of C

---
+++ inputinteger.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float  a;
+    printf("Enter The Number : ");
+    scanf("%f",&a);
+    float b;
+    b = a / 2;
+    printf("the half of number %f is : %f",a,b);
+    return 0;
+}
