01d70cdbf2cba9c73633f1312ea539a0f5fac4aa
shivanshferro <shivanshferro@gmail.com>
2026-09-18 20:41:02 +0530

    20 commit of C

--- reminder.c
+++ reminder.c
@@ -1,16 +1,27 @@
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("enter the number : ");
+//     scanf("%d",&a);
+//     printf("enter the number : ");
+//     scanf("%d",&b);
+//     int C;
+//     C = a / b;
+//     printf("%d",C);
+//     printf("\n");
+//     int z;
+//     z = a -(C*b);
+//     printf("the reminder is : %d",z);
+//     return 0;
+// }
 #include<stdio.h>
 int main(){
-    int a,b;
+    int a,b,reminder;
     printf("enter the number : ");
     scanf("%d",&a);
     printf("enter the number : ");
     scanf("%d",&b);
-    int C;
-    C = a / b;
-    printf("%d",C);
-    printf("\n");
-    int z;
-    z = a -(C*b);
-    printf("the reminder is : %d",z);
+    reminder = a % b;
+    printf("the reminder %d",reminder);
     return 0;
 }
