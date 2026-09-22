3d5ba25fbce9c9b9684541d74c2fa55cf1892db4
shivanshferro <shivanshferro@gmail.com>
2026-09-22 23:24:48 +0530

    38 commit of C

---
+++ 3positiveinteger.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("Enter the number : ");
+    scanf("%d",&x);
+    int y;
+    printf("Enter the number : ");
+    scanf("%d",&y);
+    int z;
+    printf("Enter the number : ");
+    scanf("%d",&z);
+    if(x>y && x>z){
+        printf("x is greater than both y and z");
+    }
+    if(y>x && y>z){
+        printf("y is greater than both x and z");
+    }
+    if(z>x && z>y){
+        printf("z is greater than both x and y");
+    }
+    return 0;
+}
