a0efd18b4a1febe5695cf486adab53e5cb271a62
shivanshferro <shivanshferro@gmail.com>
2026-09-23 19:51:05 +0530

    42 commit of c

--- postiveinteger.c
+++ postiveinteger.c
@@ -3,11 +3,16 @@ int main(){
     int x;
     printf("enter the number : ");
     scanf("%d",&x);
-    if(x%5==0 && x%3==0){
-        printf("yes number is divisible by 5 and 3 ");
+    if(x%5==0){
+        if(x%3==0){
+            printf("The number is  divisible by 5 and 3");
+        }
+        else{
+            printf("the number is not divisible by 5 and 3");
+        }
     }
     else{
-        printf("Not number is not divisible by 5 and 3");
+        printf("the number is not divisble by 5 and 3");
     }
     return 0;
 }
