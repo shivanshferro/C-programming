6764b3b47bde03bd66f397339e657b4468b93ef9
shivanshferro <shivanshferro@gmail.com>
2026-09-24 21:51:06 +0530

    47 commit of c

--- demo1.c
+++ demo1.c
@@ -1,27 +1,5 @@
 #include<stdio.h>
 int main(){
-    int x,y,z;
     printf("Enter the number : ");
-    scanf("%d",&x);
-    printf("Enter the number : ");
-    scanf("%d",&y);
-    printf("Enter the number : ");
-    scanf("%d",&z);
-    if(x>y){
-        if(x>z){
-            printf("x is greater than  all of three them ");
-        }
-    }
-    if(y>x){
-        if(y>z){
-            printf("y is greater than of all of three them");
-        }
-    }
-    if(z>x){
-        if(z>y){
-            printf("z is greater than all of three them");
-
-        }
-    }
     return 0;
 }
