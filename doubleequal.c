8baff8141b085f8bfd157f34800513e70e2f96c8
shivanshferro <shivanshferro@gmail.com>
2026-09-24 21:02:44 +0530

    44 commit of c

---
+++ doubleequal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("Enter the number : ");
+    scanf("%d",&x);
+    if(x==2){
+        printf("GOOD MORNING");
+    }
+    else{
+        printf("Good Night");
+    }
+    return 0;
+}
